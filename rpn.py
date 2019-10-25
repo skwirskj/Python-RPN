eb278de1942fcae470f85d1a89701f0c0fb05bf3
skwirskj <skwirskj@umich.edu>
2019-10-25 14:35:09 -0400

    Implement addition
    The calculator can now add two integers together.

--- rpn.py
+++ rpn.py
@@ -1,5 +1,16 @@
 def calculate(arg):
-    pass
+    stack = list()
+    for token in arg.split():
+        if token == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+
+        print(stack)
+    return stack.pop()
 
 def main():
     while True:
