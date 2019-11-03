ac228407e50b9f669710a19c7c6c67b5e5c1fe5a
skwirskj <skwirskj@umich.edu>
2019-11-03 16:34:12 -0500

    Adding RPN to GitHub
    This is the first commit after creating the git repo for it.
    All functions for multiply, add, divide, and substract are implemented and tested.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test:
-    python3 -m unittest
+		python3 -m unittest
 
 .PHONY: test
--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,16 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
+    def test_sub(self):
+        result = rpn.calculate('5 3 -')
+        self.assertEqual(2, result)
+    def test_mul(self):
+        result = rpn.calculate('12 8 *')
+        self.assertEqual(96, result)
+    def test_div(self):
+        result = rpn.calculate('12 8 /')
+        self.assertEqual(1, result)
+    def test_div2(self):
+        result = rpn.calculate('24 3 /')
+        self.assertEqual(8, result)
+    
